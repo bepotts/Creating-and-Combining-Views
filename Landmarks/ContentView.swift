27e6e36a85e396653ae4583cd0e3d8b2ffac2f07
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-03-08 09:26:41 -0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Brandon Potts on 3/8/20.
+//  Copyright © 2020 Brandon Potts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
